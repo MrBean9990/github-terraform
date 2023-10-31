8052b3c0ca20e5058fdca9efaa80a927ff3851da
Modestas <you@example.com>
2023-10-31 14:32:03 +0100

    fmt changes

--- Assignment2/main.tf
+++ Assignment2/main.tf
@@ -1,6 +1,6 @@
 locals {
   workspace_suffix = terraform.workspace == "default" ? "" : terraform.workspace
-  rg_name = terraform.workspace == "default" ? var.rg_name : "${var.rg_name}-${local.workspace_suffix}"
+  rg_name          = terraform.workspace == "default" ? var.rg_name : "${var.rg_name}-${local.workspace_suffix}"
 }
 
 module "keyvault" {
