7f83f168506f43924109fcc5ad7469cc6e6608d9
Modestas <you@example.com>
2023-10-31 16:13:40 +0100

    lrs -> grs

--- Assignment2/storageaccount/main.tf
+++ Assignment2/storageaccount/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name      = azurerm_resource_group.sa_rg.name
   location                 = azurerm_resource_group.sa_rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+  account_replication_type = "GRS"
   min_tls_version          = "TLS1_2"
 
 }
