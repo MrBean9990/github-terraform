090746e04e7d1e04627aaefcbceca4999cc5c755
Modestas <you@example.com>
2023-10-31 14:33:51 +0100

    change pass

--- Assignment2/variables.tf
+++ Assignment2/variables.tf
@@ -88,7 +88,7 @@ variable "vm_password" {
   type        = string
   description = "Virtual machine password"
   sensitive   = true
-  default     = "d2fSD_!EfdX12talio"
+  default     = "d2fSD_!EfdX12tTEST"
 }
 
 variable "rg_name" {
