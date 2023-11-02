5f34d5cc28f2fdbb0159548d70c3bfd974b47671
Modestas <you@example.com>
2023-11-02 17:34:53 +0100

    vnet 16

--- Assignment2/variables.tf
+++ Assignment2/variables.tf
@@ -8,7 +8,7 @@ variable "kv_location" {
 variable "kv_base_name" {
   type        = string
   description = "Name of the Key Vault"
-  default     = "keyv"
+  default     = "keyVAULT1"
 }
 
 variable "sa_base_name" {
@@ -38,7 +38,7 @@ variable "vnet_rg_location" {
 variable "vnet_name" {
   type        = string
   description = "Virtual network name"
-  default     = "vnet15"
+  default     = "vnet16"
 }
 
 variable "nsg_name" {
