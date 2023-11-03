ddfb1e08b195092c9f26011206c259d737eaccc8
Modestas <you@example.com>
2023-11-03 12:47:36 +0100

    vnet1

--- Assignment2/variables.tf
+++ Assignment2/variables.tf
@@ -41,7 +41,7 @@ variable "kv_base_name" {
 variable "vnet_name" {
   type        = string
   description = "Virtual network name"
-  default     = "vnet2"
+  default     = "vnet1"
 }
 
 variable "nsg_name" {
